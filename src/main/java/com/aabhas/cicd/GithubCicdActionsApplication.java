1150e72c2108fcdafe6a17b05def916557a40341
91982 <abhas1999.as@gmail.com>
2024-01-09 21:14:25 +0530

    Code

---
+++ src/main/java/com/aabhas/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,35 @@
+package com.aabhas.cicd;
+import org.springframework.web.bind.annotation.RestController
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+@getmapping
+
+
+
+@SpringBootApplication
+@Restcontroller
+public class GithubCicdActionsApplication {
+
+        @getmapping('/welcome')
+        public String welcome(){
+			return "Welcome to aabhas !";
+	}
+
+
+//	echo "# github-actions-example" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/Aabhas-sharma/github-actions-example.git
+//	git push -u origin main
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
