6571f496efa7324baefe32edf61cdb1c5fec62ca
91982 <abhas1999.as@gmail.com>
2024-01-10 17:37:18 +0530

    test

--- src/main/java/com/aabhas/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/aabhas/cicd/GithubCicdActionsApplication.java
@@ -2,10 +2,6 @@ package com.aabhas.cicd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-@getmapping
-
-
-
 @SpringBootApplication
 public class GithubCicdActionsApplication {
 
