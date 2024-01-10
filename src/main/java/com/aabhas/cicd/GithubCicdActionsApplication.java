151fd8e69948935aac51c958f7f45f5f8e1f4239
91982 <abhas1999.as@gmail.com>
2024-01-10 17:33:17 +0530

    test

--- src/main/java/com/aabhas/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/aabhas/cicd/GithubCicdActionsApplication.java
@@ -1,9 +1,4 @@
 package com.aabhas.cicd;
-import org.springframework.web.bind.annotation.RestController;
-
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
